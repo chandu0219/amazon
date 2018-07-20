5301e76a19392d5f7bb0dda5a1fd11e1b7c33830
chandu <chandu0219@gmail.com>
2018-07-21 00:34:41 +0100

    udate should be seen by all branches

--- ser.java
+++ ser.java
@@ -3,4 +3,4 @@ hi guys
 
 hi guts thi sline is updated by bugfix team
 
-updated by master branch team and should not visible
+updated by master branch team and should be visible both bugfix and development team
